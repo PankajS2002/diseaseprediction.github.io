14f21a09ce033acaff5660511c1f2519d2e26ad6
PankajS2002 <109162571+PankajS2002@users.noreply.github.com>
2022-11-10 23:15:10 +0530

    disease prediction system frontend

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+/* When screen is shortned and menu is clicked onclick open menu
+and when we scroll keeping that menu open onscroll closes that menu*/
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
